e9f4a99e1485a5a22d7003688e80d5fe88c85239
Diego Ferreira <diferodrigues@gmail.com>
2022-06-08 18:59:44 -0300

    viewcode study

--- ViewCode/LoginViewCode/LoginViewCode/Login/VC/ViewController.swift
+++ ViewCode/LoginViewCode/LoginViewCode/Login/VC/ViewController.swift
@@ -17,8 +17,10 @@ class ViewController: UIViewController {
     }
 
     override func viewDidLoad() {
+        let bgColor = UIColor(red: 0.22, green: 0.03, blue: 0.36, alpha: 1.00)
+        
         super.viewDidLoad()
-        self.view.backgroundColor = .red
+        self.view.backgroundColor = bgColor
     }
     
     override func viewWillAppear(_ animated: Bool) {
