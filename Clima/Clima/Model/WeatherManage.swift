a0914ecf8c5126b221e05442e252b22030d259c0
Diego Ferreira <diferodrigues@gmail.com>
2022-06-10 17:44:24 -0300

    adding clima project

---
+++ Clima/Clima/Model/WeatherManage.swift
@@ -0,0 +1,61 @@
+//
+//  WeatherManage.swift
+//  Clima
+//
+//  Created by Diego Ferreira on 10/06/22.
+//  Copyright © 2022 App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct WeatherManage {
+    let weatherURL = "https://api.openweathermap.org/data/2.5/weather?appid=215b772bcca3e58f5a3a3d6d740cf69e&units=metric"
+    
+    func fetchWeather(cityName: String) {
+        let urlString = "\(weatherURL)&q=\(cityName)"
+        performRequest(urlString: urlString)
+    }
+    
+    func performRequest(urlString: String) {
+        //1. Create a URL
+        if let url = URL(string: urlString) {
+            //2. Create a URLSession
+            let session = URLSession(configuration: .default)
+            
+            //3. Give the session a task
+            let task = session.dataTask(with: url) { (data, response, error) in
+                if error != nil {
+                    print(error!)
+                    return
+                }
+                
+                if let safeData = data {
+                    self.parseJSON(weatherData: safeData)
+                }
+            }
+            
+            //4. Start the task
+            task.resume()
+        }
+    }
+    
+    func parseJSON(weatherData: Data) {
+        let decoder = JSONDecoder()
+        
+        do {
+            let decodedData = try decoder.decode(WeatherData.self, from: weatherData)
+            let id = decodedData.weather[0].id
+            let temp = decodedData.main.temp
+            let name = decodedData.name
+            
+            let weather = WeatherModel(conditionId: id, cityName: name, temperature: temp)
+            
+            print(weather.conditionName)
+            
+        } catch {
+            print(error)
+        }
+    }
+    
+
+}
