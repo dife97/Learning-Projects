ec5b27e7e91151d472fe1cfdb96c49e919617f9c
Diego Ferreira <diferodrigues@gmail.com>
2022-06-14 22:56:35 -0300

    bytecoin

--- ByteCoin/ByteCoin/Model/CoinManager.swift
+++ ByteCoin/ByteCoin/Model/CoinManager.swift
@@ -9,11 +9,35 @@
 import Foundation
 
 struct CoinManager {
-    
     let baseURL = "https://rest.coinapi.io/v1/exchangerate/BTC"
     let apiKey = "YOUR_API_KEY_HERE"
     
     let currencyArray = ["AUD", "BRL","CAD","CNY","EUR","GBP","HKD","IDR","ILS","INR","JPY","MXN","NOK","NZD","PLN","RON","RUB","SEK","SGD","USD","ZAR"]
-
     
+    func getCoinPrice(for currency: String) {
+        //1. Create a URL
+        let urlString = "https://rest.coinapi.io/v1/exchangerate/BTC/USD?apikey=FB720474-0DD1-4C3F-832A-0945EF230F72"
+        
+        if let url = URL(string: urlString) {
+            //2. Create a URLSession
+            let session = URLSession(configuration: .default)
+            
+            //3. Give the session a task
+            let task = session.dataTask(with: url) { (data, response, error) in
+                if error != nil {
+                    self.delegate?.didFailWithError(error: error!)
+                    return
+                }
+                
+                if let safeData = data {
+                    if let weather = self.parseJSON(safeData) {
+                        self.delegate?.didUpdateWeather(self, weather: weather)
+                    }
+                }
+            }
+            
+            //4. Start the task
+            task.resume()
+        }
+    }
 }
