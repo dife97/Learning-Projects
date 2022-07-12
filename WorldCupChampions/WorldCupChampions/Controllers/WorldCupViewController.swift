4a23a43d122c9f015d7284910b52dc4b22d734c0
Diego Ferreira <diferodrigues@gmail.com>
2022-07-12 14:55:54 -0300

    add WorldCupChampions project

---
+++ WorldCupChampions/WorldCupChampions/Controllers/WorldCupViewController.swift
@@ -0,0 +1,18 @@
+//
+//  WolrdCupViewController.swift
+//  WorldCupChampions
+//
+//  Created by Diego Ferreira on 12/07/22.
+//
+
+import UIKit
+
+class WorldCupViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+}
